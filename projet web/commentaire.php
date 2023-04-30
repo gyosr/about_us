9ddda5615f5615eacae0c9aed694f41c09cbf76d
yosr <131818632+gyosr@users.noreply.github.com>
2023-04-30 01:35:19 +0100

    code1
    1ststep

---
+++ projet web/commentaire.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>about us </title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet"  href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css"/>
+  
+</head>
+<body>
+    <header>
+       <div class="principale">
+        <div class="logo">
+            <a href="#"><span>E</span>xp<span>l</span>oria</a>
+        </div>
+       </div>
+       <ul>
+        <li >
+            <a href="#">acceuil</a>
+        </li>
+        <li>
+            <a href="#">service</a>
+        </li>
+        <li>
+            <a href="#">blog</a>
+        </li>
+        <li class="active">
+            <a href="#">a propos de nous</a>
+        </li>
+        <li>
+            <a href="#">contact</a>
+        </li>
+       </ul></div>
+       <div class="titre">
+        <h1>A propos de nous </h1>
+       </div>
+    </header>
+   
+   
+    <!--review section-->
+    <section class="review" id="review">
+         <h1 class="heading">
+            review
+         </h1>
+         <div class="swiper review-slider">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                    <div class="box">
+                        <img src="./photo/blank-profile-picture-g1e9371293_1280.png ">
+                        <h3>john deo </h3>
+                        <p> le commentaire de john j'ai aime mon voyage qui a ete incroyable jai passe un voyage inoubliable et j'ai profite de chaque moment merci pour votre professionnalisme  </p>
+                        <div class="stars">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="far fa-star"></i>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="swiper-slide">
+                    <div class="box">
+                        <img src="./photo/blank-profile-picture-g1e9371293_1280.png ">
+                        <h3>yasmine ferchichi</h3>
+                        <p> le commentaire de yasmine j'ai aime mon voyage qui a ete incroyable jai passe un voyage inoubliable et j'ai profite de chaque moment merci pour votre professionnalisme </p>
+                            
+                        <div class="stars">
+                           <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="far fa-star"></i>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="swiper-slide">
+                    <div class="box">
+                        <img src="./photo/blank-profile-picture-g1e9371293_1280.png ">
+                        <h3>john deo </h3>
+                        <p>le commentaire de john j'ai aime mon voyage qui a ete incroyable jai passe un voyage inoubliable et j'ai profite de chaque moment merci pour votre professionnalisme </p>
+                        <div class="stars">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="far fa-star"></i>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="swiper-slide">
+                    <div class="box">
+                        <img src="./photo/blank-profile-picture-g1e9371293_1280.png">
+                        <h3>john deo </h3>
+                        <p>le commentaire de john j'ai aime mon voyage qui a ete incroyable jai passe un voyage inoubliable et j'ai profite de chaque moment merci pour votre professionnalisme</p>
+                        <div class="stars">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="far fa-star"></i>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="swiper-slide">
+                    <div class="box">
+                        <img src="./photo/blank-profile-picture-g1e9371293_1280.png ">
+                        <h3>john deo </h3>
+                        <p> le commentaire de john j'ai aime mon voyage qui a ete incroyable jai passe un voyage inoubliable et j'ai profite de chaque moment merci pour votre professionnalisme </p>
+                        <div class="stars">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="far fa-star"></i>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
+    <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
+    <script src="script.js"></script>
+  <?php
+
+
+
+     ?>
+</body>
+</html>
