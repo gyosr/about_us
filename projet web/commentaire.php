85a5273a5bdf9b085be87116e4a02658f8da9717
yosr <131818632+gyosr@users.noreply.github.com>
2023-05-05 01:41:40 +0100

    s3

--- projet web/commentaire.php
+++ projet web/commentaire.php
@@ -12,7 +12,7 @@
 </head>
 <body>
     <header>
-       <div class="principale">
+       <div class="principale" >
         <div class="logo">
             <a href="#"><span>E</span>xp<span>l</span>oria</a>
         </div>
@@ -38,6 +38,20 @@
         <h1>A propos de nous </h1>
        </div>
     </header>
+    <!--en savoir plus-->
+    <section class="home-about">
+        <div class="image">
+            <img src="photo/voyage2.jpg">
+        </div>
+        <div class="content">
+            <h3>A propos De Nous</h3>
+            <p>Vous cherchez à voyager ? Nous sommes là pour vous aider ! Notre agence de voyage propose des destinations de rêve, des conseils d'experts et des services personnalisés pour vous aider à planifier le voyage parfait. Nous sommes passionnés par les voyages et nous voulons partager cette passion avec vous. Nous nous efforçons de vous offrir une expérience de voyage inoubliable et nous nous engageons à vous aider à créer des souvenirs qui dureront toute une vie.
+
+                Faites-nous confiance pour votre prochaine aventure et laissez-nous vous aider à réaliser votre rêve de voyage.Cliquez ici dès maintenant pour planifier votre prochaine aventure !</p>
+            <a href ="#" class="btn">En savoir plus</a>
+
+        </div>
+    </section>
    
    
     <!--review section-->
@@ -127,12 +141,99 @@
 
         </div>
     </section>
+   
+
+
+    <div class="commentaires">
+        <h2>Ajouter un commentaire</h2>
+        <form action="traitement_commentaire.php" method="post">
+            <label for="nom">Nom :</label>
+            <input type="text" id="nom" name="nom"><br>
+            <div class="rating">
+                <input type="radio" name="rating" id="star1" value="1">
+                <label for="star1">&#9733;</label>
+                <input type="radio" name="rating" id="star2" value="2">
+                <label for="star2">&#9733;</label>
+                <input type="radio" name="rating" id="star3" value="3">
+                <label for="star3">&#9733;</label>
+                <input type="radio" name="rating" id="star4" value="4">
+                <label for="star4">&#9733;</label>
+                <input type="radio" name="rating" id="star5" value="5">
+                <label for="star5">&#9733;</label>
+              </div>
+            <label for="commentaire">Commentaire :</label>
+            <textarea id="commentaire" name="commentaire"></textarea><br>
+            <input type="submit" value="Envoyer">
+        </form>
+    </div>
+    <?php
+// Connexion à la base de données
+$servername = "localhost";
+$username = "yosr";
+$password = "mot_de_passe";
+$dbname = "espace_commentaire";
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Vérification de la connexion
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Récupération du commentaire saisi par l'utilisateur
+if(isset($_POST['commentaire'])) {
+    $commentaire = $_POST['commentaire'];
+} else {
+    $commentaire = "";
+}
+
+// Requête SQL pour ajouter le commentaire dans la table "commentaires"
+$sql = "INSERT INTO commentaires (commentaire) VALUES ('$commentaire')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Commentaire ajouté avec succès";
+} else {
+    echo "Erreur lors de l'ajout du commentaire: " . $conn->error;
+}
+
+// Fermeture de la connexion
+$conn->close();
+?>
+
+
+    <section class="local">
+        <h1> 
+         Vous pouvez nous trouvez !
+        </h1>
+        <p>Nos localisations se trouvent  </p>
+        <div class="directoryPresentation">
+    
+            <img src="photo\carte.png" alt="Carte" >
+        </div>
+        <div class="row">
+            
+           <div class="local-col">
+              <img src="photo\tour-dhorloge.jpg" >
+             <div class="layer">
+                <h3>Tunis</h3>
+             </div>
+             </div>
+             <div class="local-col">
+                <img src="photo\sousse.jpg"  >
+               <div class="layer">
+                  <h3>Sousse</h3>
+               </div>
+             </div>
+               <div class="local-col">
+                <img src="photo\sfax.jpg"  >
+               <div class="layer">
+                  <h3>Sfax </h3>
+               </div>
+           </div>
+        </div>
+    </section>
+
     <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
     <script src="script.js"></script>
-  <?php
 
-
-
-     ?>
 </body>
 </html>
